0d59acf936ff9701e2dcebb0cd44485abf385196
Victor <Victorphoswa@gmail.com>
2022-12-13 08:26:58 +0200

    modified the certificate div img

--- Jscript.js
+++ Jscript.js
@@ -1,48 +1,53 @@
 let shiftDown = document.getElementById("btn1i");
 let shiftUp = document.getElementById("btn2i");
-
 let CertDiv = document.getElementById("certi");
+let menu = document.getElementById("hamburger");
+let menuu = document.getElementById("menu1");
+let nav = document.getElementById("nav-wrap");
 
 const CertSlider = ["/images/RIntro.png", "/images/VisualisationR-1.png"];
 
 let i = 0;
 
-
+//Displaying the first img in the certificate slider
+CertDiv.src = CertSlider[i];
 
 /*
 Scroll slider button functions
 the functions index every picture in the array
 */
-function shiftdown(){
+
+function SlideUp(){
+    //Assert length of the image slider array
+    if(i < CertSlider.length && i != CertSlider.length-1){
+        i+=1;
+        CertDiv.src = CertSlider[i];
+    }
     
-    if(i > 0 && i <=  CertSlider.length)
-    {
-        
+}
+
+
+function SlideDown(){
+    //Assert length of the image slider array
+    if(i > 0){
         i-=1;
         CertDiv.src = CertSlider[i];
-    } 
-    else
-    {
-        i = i;
     }
-        
-}
-
-function shiftup(){
-
     
-    if(i >= 0 && i < CertSlider.length)
-    {
-        CertDiv.src = CertSlider[i];
-        i+=1;
-    }
-    else
-    {
-        i = i;
-    }
+    
 }
 
-shiftDown.addEventListener('click', shiftdown,true);
+function MenuDisplay(){
+    menuu.style.display = "flex";
+    
+}
 
-shiftUp.addEventListener('click', shiftup,true);
+
+
+//Adding click event listners to the scroll btn
+shiftUp.addEventListener('click', SlideUp,true);
+shiftDown.addEventListener('click', SlideDown,true);
+menu.addEventListener('click', MenuDisplay,true);
+
+
 
